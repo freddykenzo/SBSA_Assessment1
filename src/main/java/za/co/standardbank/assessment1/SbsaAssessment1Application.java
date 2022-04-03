865b45596b2517b3bc61f84a709d1ff9fe94a17b
Freddy Sop Mboudjieka <FSopMboudjieka@jhb.dvt.co.za>
2022-04-03 23:34:36 +0200

    initial commit

---
+++ src/main/java/za/co/standardbank/assessment1/SbsaAssessment1Application.java
@@ -0,0 +1,13 @@
+package za.co.standardbank.assessment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbsaAssessment1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbsaAssessment1Application.class, args);
+    }
+
+}
---
+++ src/test/java/za/co/standardbank/assessment1/SbsaAssessment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.standardbank.assessment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbsaAssessment1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
